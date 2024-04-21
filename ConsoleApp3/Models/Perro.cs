800c5237f242209a3241c0decfc53c0753857cd6
Emmanuel Rodríguez <emmanuel15072005@outlook.com>
2024-04-20 23:05:51 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleApp3/Models/Perro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3.Models
+{
+    internal class Perro : animales
+    {
+        public override void sonidoAnimal()
+        {
+            Console.WriteLine("El perro hace guau guau");
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleApp3.Models;
+using System;
+
+namespace Poo
+{
+    class Program
+    {
+        static void Main()
+        {
+            animales miperro = new Perro();
+            animales migato = new Gato();
+
+
+            miperro.sonidoAnimal();
+            migato.sonidoAnimal();
+        }
+    }
+}
